9fce9d568757055f1b53dc6ec1b9b0999d4816e1
G186482021 <218648st.takushoku-u.ac.jp>
2023-06-24 09:01:17 +0900

    a

---
+++ kazuate.js
@@ -0,0 +1,38 @@
+// 答え
+let kotae = Math.floor(Math.random()*10) + 1;
+console.log('答え（デバッグ用）: ' + kotae);
+
+// 入力回数（予想回数）
+let kaisu = 0;
+
+// 予想を4回実行する
+// 将来以下の hantei(); の4回の呼び出しを全て削除する
+// 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
+hantei();
+hantei();
+hantei();
+hantei();
+
+// ボタンを押した後の処理をする関数 hantei() の定義
+function hantei() {
+  // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
+  let yoso = 4;
+  
+  // 課題3-1: 正解判定する
+  // kotae と yoso が一致するかどうか調べて結果を出力
+  // 課題3-1における出力先はコンソ
+  var lists = [1, 2, 3, 4, 5];
+ 
+for( var kaisu of lists ) {
+console.log(kaisu + "回目の予想: " + 4);
+if (kaisu > 4) {
+     console.log("答えは" + kotae + "です。回数制限");
+} else if (kotae === 4 ) {
+        console.log("正解です．おめでとう!");
+    } else if (kotae > 4){
+            console.log("まちがい、もっとおおきく!");
+    } else {
+            console.log("まちがい、もっとちいさく!");
+    }
+}
+}
