07f44474040ff0ca3e5486c316e36cc7e43c7975
G186482021 <218648st.takushoku-u.ac.jp>
2023-07-19 23:12:41 +0900

    a

--- wether.js
+++ wether.js
@@ -49,3 +49,78 @@ let data = {
 
 
   console.log(data);
+
+
+  let div = document.querySelector('div#result');
+
+
+
+
+  let a1 = document.createElement('h1');
+  a1.textContent = '北京市';  
+  div.insertAdjacentElement('beforeend', a1);
+
+
+
+
+  let a2 = document.createElement('h2');
+  a2.textContent = '-天気-';  
+  div.insertAdjacentElement('beforeend', a2);
+
+  let b1 = document.createElement('p');
+  b1.textContent = '曇りがち';  
+  div.insertAdjacentElement('beforeend', b1);
+
+
+
+  let a3 = document.createElement('h2');
+  a3.textContent = '-気温-';  
+  div.insertAdjacentElement('beforeend', a3);
+
+
+  let a4 = document.createElement('h3');
+  a4.textContent = '最高';  
+  div.insertAdjacentElement('beforeend', a4);
+
+  let b2 = document.createElement('p');
+  b2.textContent = '9.94';  
+  div.insertAdjacentElement('beforeend', b2);
+
+
+  let a5 = document.createElement('h3');
+  a5.textContent = '最低';  
+  div.insertAdjacentElement('beforeend', a5);
+
+  let b3 = document.createElement('p');
+  b3.textContent = '9.94';  
+  div.insertAdjacentElement('beforeend', b3);
+
+
+
+  let a6 = document.createElement('h2');
+  a6.textContent = '-湿度-';  
+  div.insertAdjacentElement('beforeend', a6);
+
+  let b4 = document.createElement('p');
+  b4.textContent = '14';  
+  div.insertAdjacentElement('beforeend', b4);
+
+
+
+  let a7 = document.createElement('h2');
+  a7.textContent = '-風速・風向き-';  
+  div.insertAdjacentElement('beforeend', a7);
+
+  let b5 = document.createElement('p');
+  b5.textContent = '2.65・197';  
+  div.insertAdjacentElement('beforeend', b5);
+
+
+
+  let a8 = document.createElement('h2');
+  a8.textContent = '-緯度・経度-';  
+  div.insertAdjacentElement('beforeend', a8);
+
+  let b6 = document.createElement('p');
+  b6.textContent = '39.9075・116.3972';  
+  div.insertAdjacentElement('beforeend', b6);
