73b1f681bc820086277bff63a028cd4260aa3b96
G186482021 <218648st.takushoku-u.ac.jp>
2023-07-20 00:57:29 +0900

    a

--- wether.js
+++ wether.js
@@ -124,3 +124,21 @@ let data = {
   let b6 = document.createElement('p');
   b6.textContent = '39.9075・116.3972';  
   div.insertAdjacentElement('beforeend', b6);
+
+
+
+  
+  let x = document.querySelector('button#btn');
+x.addEventListener('click', showSelectResult);
+
+function showSelectResult() {
+    let s = document.querySelector('select#santaro');
+    let idx = s.selectedIndex;  // idx 番目の option が選択された
+
+    let os = s.querySelectorAll('option');  // s の子要素 option をすべて検索
+    let o = os.item(idx);       // os の idx 番目の要素
+
+    console.log('選択された ' + idx + ' 番目の option の情報:');
+    console.log('  value=' + o.getAttribute('value'));  // id 属性を表示
+    console.log('  textContent='+o.textContent);
+}
